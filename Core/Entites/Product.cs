2e190a28f6c4f6d44f2623b7b9eee0a1bdf8be01
Nizar Al_abadi <nizar12abadi@gmail.com>
2024-04-17 18:36:27 +0300

    creating API,Core and Infrastructure

---
+++ Core/Entites/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entites
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+    }
+}
