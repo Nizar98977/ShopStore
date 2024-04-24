78b2cceb275d013e25dd427a1b48f690326453a3
Nizar Al_abadi <nizar12abadi@gmail.com>
2024-04-24 22:04:39 +0300

    Adding AutoMapper & Images for the Products

---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,23 @@
+﻿using API.DTOs;
+using AutoMapper;
+using Core.Entites;
+
+namespace Skinet.API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _configuration;
+        public ProductUrlResolver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _configuration["ApiURl"] + source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}
