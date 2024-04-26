d81fa2ef914251165d29136a6dddc271340dc350
Nizar Al_abadi <nizar12abadi@gmail.com>
2024-04-26 16:11:22 +0300

    fix the DataMigration issue

--- Infrastructure/Migrations/20240426130855_fixdata.cs
+++ Infrastructure/Migrations/20240426130855_fixdata.cs
@@ -5,7 +5,7 @@
 namespace Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class addIsDeleted : Migration
+    public partial class fixdata : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
